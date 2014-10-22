fa0d866602d0c7b5e7b3bcce44dab766014d3e48
johnsonlee <g.johnsonlee@gmail.com>
2014-10-22 11:44:10 +0800

    Update Android.mk

--- sdk/Android.mk
+++ sdk/Android.mk
@@ -2,17 +2,21 @@ LOCAL_PATH := $(call my-dir)
 
 
 include $(CLEAR_VARS)
+LOCAL_MODULE        := com.smartkey.umeng.sdk.libbspatch
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_TAG    := optional
+LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
+LOCAL_SRC_FILES     := $(LOCAL_PATH)/libs/armeabi/libbspatch.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
     umeng-analytics:libs/umeng-analytics-v5.2.4.jar \
     umeng-fb:libs/umeng-fb-v4.3.2.jar \
     umeng-message:libs/umeng-message-v1.3.0.jar \
     umeng-update:libs/umeng-update-v2.4.2.jar
 include $(BUILD_MULTI_PREBUILT)
-
-
-#include $(CLEAR_VARS)
-#LOCAL_PREBUILT_LIBS := $(LOCAL_PATH)/libs/armeabi/libbspatch.so
-#include $(BUILD_MULTI_PREBUILT)
 
 
 include $(CLEAR_VARS)
